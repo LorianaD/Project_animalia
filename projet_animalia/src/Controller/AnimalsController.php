82edbfb3b6c91d6016aeb9c24c94e4db79cbbef2
LorianaD <loriana.diano@gmail.com>
2025-09-29 16:42:01 +0200

    upload

--- projet_animalia/src/Controller/AnimalsController.php
+++ projet_animalia/src/Controller/AnimalsController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Repository\AnimalsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AnimalsController extends AbstractController
 {
-    #[Route('/animals', name: 'app_animals')]
-    public function index(): Response
+    #[Route('/animals', name: 'animals')]
+    public function index(AnimalsRepository $AnimalsRepository): Response
     {
+
+        $animals = $AnimalsRepository->findAll();
+
         return $this->render('animals/index.html.twig', [
             'controller_name' => 'AnimalsController',
+            'animals' => $animals
+        ]);
+    }
+
+    #[Route('/animals/add', name: 'add_animals')]
+    public function add(): Response
+    {
+
+        
+
+        return $this->render('animals/add.html.twig', [
+            'titre' => "Ajout d'un animal",
         ]);
     }
 }
