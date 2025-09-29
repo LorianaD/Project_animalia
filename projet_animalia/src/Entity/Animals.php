7ea3937ab596b52f9261f2217743a69c45058ba9
VanessDev <vanessa.biamonti@gmail.com>
2025-09-29 16:25:21 +0200

    upload5

--- projet_animalia/src/Entity/Animals.php
+++ projet_animalia/src/Entity/Animals.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\AnimalsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -25,6 +27,23 @@ class Animals
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $img = null;
+
+    #[ORM\ManyToOne]
+    private ?Type $type_id = null;
+
+    #[ORM\ManyToOne]
+    private ?Genre $genre_id = null;
+
+    /**
+     * @var Collection<int, Reviews>
+     */
+    #[ORM\OneToMany(targetEntity: Reviews::class, mappedBy: 'animals')]
+    private Collection $review;
+
+    public function __construct()
+    {
+        $this->review = new ArrayCollection();
+    }
 
 
 
@@ -77,6 +96,60 @@ class Animals
     public function setImg(string $img): static
     {
         $this->img = $img;
+
+        return $this;
+    }
+
+    public function getTypeId(): ?Type
+    {
+        return $this->type_id;
+    }
+
+    public function setTypeId(?Type $type_id): static
+    {
+        $this->type_id = $type_id;
+
+        return $this;
+    }
+
+    public function getGenreId(): ?Genre
+    {
+        return $this->genre_id;
+    }
+
+    public function setGenreId(?Genre $genre_id): static
+    {
+        $this->genre_id = $genre_id;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Reviews>
+     */
+    public function getReview(): Collection
+    {
+        return $this->review;
+    }
+
+    public function addReview(Reviews $review): static
+    {
+        if (!$this->review->contains($review)) {
+            $this->review->add($review);
+            $review->setAnimals($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReview(Reviews $review): static
+    {
+        if ($this->review->removeElement($review)) {
+            // set the owning side to null (unless already changed)
+            if ($review->getAnimals() === $this) {
+                $review->setAnimals(null);
+            }
+        }
 
         return $this;
     }
--- projet_animalia/src/Entity/Reviews.php
+++ projet_animalia/src/Entity/Reviews.php
@@ -19,6 +19,9 @@ class Reviews
     #[ORM\Column(length: 1000)]
     private ?string $review = null;
 
+    #[ORM\ManyToOne(inversedBy: 'review')]
+    private ?Animals $animals = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -44,6 +47,18 @@ class Reviews
     public function setReview(string $review): static
     {
         $this->review = $review;
+
+        return $this;
+    }
+
+    public function getAnimals(): ?Animals
+    {
+        return $this->animals;
+    }
+
+    public function setAnimals(?Animals $animals): static
+    {
+        $this->animals = $animals;
 
         return $this;
     }
